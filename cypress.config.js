faa8a8fc83c90f80560e56ec671be61b755d3d01
acs-pwr <ardiancahyaseptianto99@gmail.com>
2023-08-15 11:47:43 +0700

    initial commit

---
+++ cypress.config.js
@@ -0,0 +1,25 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  reporter: "cypress-multi-reporters",
+  reporterOptions: {
+    configFile: "reporter-config.json",
+  },
+
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  video: false,
+  env: {
+    username: "",
+    password: "",
+    apiUrl: "",
+  },
+
+  e2e: {
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+    excludeSpecPattern: ["**/1-getting-started/*", "**/2-advanced-examples/*"],
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
